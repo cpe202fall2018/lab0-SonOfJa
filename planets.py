587943adf8488b1e24dd82f38720b0dc1b7af522
jtran200 <jtran200@calpoly.edu>
2018-09-22 01:06:39 -0700

    implemented planets.py correctly

--- planets.py
+++ planets.py
@@ -1,7 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 22 00:33:38 2018
+
+@author: Jason Tran
+"""
+
 def weight_on_planets():
-   # write your code here
-   earthWeight = int(input('What do you weigh on earth? '))
-   print('\nOn Mars you would weigh ' + str(earthWeight*.38) + ' pounds.\nOn Jupiter you would weigh ' + str(earthWeight*2.34) + ' pounds.')
+    # write your code here
+    earthWeight = int(input())
+    print('What do you weigh on earth? \nOn Mars you would weigh ' + str(earthWeight*.38) + ' pounds.\nOn Jupiter you would weigh ' + str(earthWeight*2.34) + ' pounds.')
    
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
--- planets_test.py
+++ planets_test.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 22 00:37:32 2018
+
+@author: Jason Tran
+"""
+
 import unittest
 import io, sys
 from planets import *
@@ -9,9 +16,8 @@ class Test_planets(unittest.TestCase):
         sys.stdout = student_output = io.StringIO()        
         expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
         weight_on_planets()
-        #print(student_output.getvalue().strip())
+#        print(student_output.getvalue().strip())
         self.assertEqual(expected_out, student_output.getvalue().strip())
-
 
 if __name__ == "__main__":
         unittest.main()
