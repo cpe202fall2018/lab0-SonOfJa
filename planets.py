29e8f54a2e8b8ffee407577afc9ec9eb8b9339bf
jtran200 <jtran200@calpoly.edu>
2018-09-23 14:01:14 -0700

    Commented planets.py code

--- planets.py
+++ planets.py
@@ -6,6 +6,9 @@ Created on Sat Sep 22 00:33:38 2018
 """
 
 def weight_on_planets():
+    """
+    Reads input (user weight on Earth) and prints corresponding weights on Mars and Jupiter.
+    """
     # write your code here
     earthWeight = int(input())
     print('What do you weigh on earth? \nOn Mars you would weigh ' + str(earthWeight*.38) + ' pounds.\nOn Jupiter you would weigh ' + str(earthWeight*2.34) + ' pounds.')
